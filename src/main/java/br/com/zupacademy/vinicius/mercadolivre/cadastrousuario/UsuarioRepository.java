4109c2a17f0272331a751a3f77151565a6c0a2c2
Vinícius de França <vgf.vinicius@gmail.com>
2021-05-25 18:25:57 -0300

    Revert "validacao email"
    This reverts commit 65cf1451f98d174bcc965af0afaabc80089f86d7.

--- src/main/java/br/com/zupacademy/vinicius/mercadolivre/cadastrousuario/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.zupacademy.vinicius.mercadolivre.cadastrousuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
